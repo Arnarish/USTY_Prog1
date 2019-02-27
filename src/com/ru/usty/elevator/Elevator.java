148f139079609dd98caf02e57859163af900114e
Arnar Elvarsson <Arnar91@gmail.com>
2019-02-27 14:46:33 +0000

    Added some initial functionality to Person and ElevatorScene, progress

--- src/com/ru/usty/elevator/Elevator.java
+++ src/com/ru/usty/elevator/Elevator.java
@@ -5,15 +5,15 @@ import java.util.concurrent.Semaphore;
 
 public class Elevator implements Runnable{
 	int currFloor, maxFloor, passangers;
-	Vector<Person> passangersInLift;
-	Semaphore doorLock;
+	//Vector<Person> passangersInLift;
+	//Semaphore doorLock;
 	
-	public Elevator(int currFloor, int maxFloor, int passangersTotal, Vector<Person> passangersInLift, Semaphore doorLock) {
+	public Elevator(int currFloor, int maxFloor, int passangersTotal/*, Vector<Person> passangersInLift, Semaphore doorLock*/) {
 		this.currFloor = currFloor;
 		this.maxFloor = maxFloor;
 		this.passangers = passangersTotal;
-		this.passangersInLift = passangersInLift;
-		this.doorLock = doorLock;
+		//this.passangersInLift = passangersInLift;
+		//this.doorLock = doorLock;
 		
 		
 		
